e0cbf52e675a17634e586fa99d17abf3c36d377b
ripsquadd <harwester1337@gmail.com>
2022-11-17 13:35:54 +0700

    Сделана форма добавления запроса, удаление не работает, надо еще фильтр

--- mysite/webportal/urls.py
+++ mysite/webportal/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('accounts/profile/', profile, name='profile'),
     path('accounts/logout/', WPLogoutView.as_view(), name='logout'),
     path('accounts/register/', WPRegisterViews.as_view(), name='register'),
-    path('accounts/profile/<pk>', delete, name='delete'),
+    path('accounts/profile/#', delete, name='delete'),
     path('accounts/create/', create, name='create'),
     path('accounts/profile/new', profile_status_new, name='new'),
 ]
